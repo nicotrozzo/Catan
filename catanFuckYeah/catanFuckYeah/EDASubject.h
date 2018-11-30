fac9005d04a4cef7f01aa6b9ca7140a8ca2dbe70
gonzalo Davidov <gdavidov@itba.edu.ar>
2018-11-30 11:56:36 -0300

    qq

---
+++ catanFuckYeah/catanFuckYeah/EDASubject.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "observer.h"
+#include <list>
+
+class EDASubject
+{
+
+public:
+	EDASubject() {}
+	virtual ~EDASubject() {}
+	void attach(observer * observerPtr);
+	void detach(observer * observerPtr);
+
+protected:
+	void notifyAllObservers(void);
+
+private:
+	list <observer * > listOfObservers;
+
+};
