c79c2590b7deb7f67f5b0f4ca4b053762e17764c
gonzalo Davidov <gdavidov@itba.edu.ar>
2018-11-30 15:15:44 -0300

    yu

--- catanFuckYeah/catanFuckYeah/catanGameModel.h
+++ catanFuckYeah/catanFuckYeah/catanGameModel.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "EDASubject.h"
+#include "bossFSMEvents.h"
 
 class catanGameModel : public EDASubject
 {
 public:
 	catanGameModel();
-
 	~catanGameModel();
-
+	bool dicesThrown(unsigned int diceValue);	//modifica informacion del juego
+	bool construction(networkingEventTypes type, string coords);		//devuelve false si la construccion no es valida, PUEDE MODIFICAR GAME OVER
+	bool playersTrade();
+	bool bankTrade();
+	bool gameOver();	//true si termino el juego, llamar despues de construccion!!!!!!!!
 private:
 	
 };
