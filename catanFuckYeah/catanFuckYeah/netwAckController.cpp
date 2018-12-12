283744777831d33744ce16fc01f01ee2215be963
Federico Tondi <fedetondi@gmail.com>
2018-12-12 20:11:34 -0300

    jopgwwr8g4hifwn49pg349

--- catanFuckYeah/catanFuckYeah/netwAckController.cpp
+++ catanFuckYeah/catanFuckYeah/netwAckController.cpp
@@ -4,11 +4,20 @@
 netwAckController::netwAckController(catanGameModel * game)
 {
 	gameModel = game;
-	numberOfCalls = 0;
-	phases = F_PHASE;
-	amountOfAck += ((gameModel->getOtherPlayer().getAmountOfCards() < 7) ? 1 : 0);
-	amountOfAck += ((gameModel->getCurrentPlayer().getAmountOfCards() > 7) ? 1 : 0);
-	amountOfAck += 1;		//le sumo el ack recibido por robber_move
+	initial = 0;
+	init();
+}
+
+void netwAckController::init()
+{
+	if (!initial)
+	{
+		phases = I_PHASE;
+		amountOfAckExpected += ((gameModel->getOtherPlayer().getAmountOfCards() < 7) ? 1 : 0);
+		amountOfAckExpected += ((gameModel->getCurrentPlayer().getAmountOfCards() > 7) ? 1 : 0);
+		amountOfAckExpected += 1;		//le sumo el ack recibido por robber_move
+		initial = 1;
+	}
 }
 
 bool netwAckController::parseNetworkingEvent(networkingEv * ev)
@@ -19,6 +28,7 @@ bool netwAckController::parseNetworkingEvent(networkingEv * ev)
 		switch (action)
 		{
 		case ROBBER_CASE:
+			init();
 			/*if (gameModel->isPlayer1Playing())
 			{
 				if (amountOfAck)
@@ -27,28 +37,38 @@ bool netwAckController::parseNetworkingEvent(networkingEv * ev)
 				}
 				
 			}*/
-			switch (numberOfCalls)
+			if (checkPhase())
 			{
-			case 0:
-				if (gameModel->getOtherPlayer().getAmountOfCards() > 7)
+				switch (phases)
 				{
-					
+				case F_PHASE: //generar evento que se quede en el mismo lugar
+					amountOfAckExpected--;
+					break;
+				case S_PHASE:	//generar evento que se quede en el mismo lugar
+					amountOfAckExpected--;
+					break;
+				case T_PHASE:
+					initial = 0;
+					if (amountOfAckExpected == 0)
+					{
+						controllerEvent = new playingFSMEvent(CHANGE_STATE);
+					}
+					else
+					{
+						controllerEvent = new playingFSMErrorEv("Error reciving ack in robber final stage");
+					}
+					break;
+				default:
+					break;
 				}
-				break;
-			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			default:
-				break;
 			}
-			numberOfCalls++;
-			
+			else
+			{
+				controllerEvent = new playingFSMErrorEv("Error reciving ack in robber stage");
+			}
 			break;
 		case OTHER_CASE:
-			controllerEvent = new playingFSMEv(CHANGE_STATE);
+			controllerEvent = new playingFSMEvent(CHANGE_STATE);
 			break;
 		}
 	}
@@ -59,13 +79,55 @@ bool netwAckController::setAction(cases action_)
 {
 	if (validAction(action_))
 	{
-		action = action_
+		action = action_;
 	}
 }
 
 bool netwAckController::validAction(cases action_)
 {
 	return (action_ >= ROBBER_CASE && action_ <= OTHER_CASE);
+}
+
+bool netwAckController::checkPhase()
+{
+	bool ret = false;
+	if ((amountOfAckExpected == 3) && (phases == I_PHASE))
+	{
+		ret = true;
+	}
+	else if ((amountOfAckExpected == 2) && (phases == F_PHASE))
+	{
+		ret = true;
+	}
+	else if ((amountOfAckExpected == 1) && (phases == S_PHASE))
+	{
+		ret = true;
+	}
+	else
+	{
+		ret = false;
+	}
+	return ret;
+}
+
+void netwAckController::setPhase()
+{
+	if ((gameModel->getOtherPlayer().getAmountOfCards() > 7) && (phases == I_PHASE))
+	{
+		phases == F_PHASE;
+	}
+	else if ((gameModel->getOtherPlayer().getAmountOfCards() <= 7) && (phases == I_PHASE) && (gameModel->getCurrentPlayer().getAmountOfCards() > 7))
+	{
+		phases = S_PHASE;
+	}
+	else if ((gameModel->getCurrentPlayer().getAmountOfCards() > 7) && (phases == F_PHASE))
+	{
+		phases = S_PHASE;
+	}
+	else if ((gameModel->getCurrentPlayer().getAmountOfCards() <= 7) && (phases == F_PHASE))
+	{
+		phases = T_PHASE;
+	}
 }
 
 netwAckController::~netwAckController()
