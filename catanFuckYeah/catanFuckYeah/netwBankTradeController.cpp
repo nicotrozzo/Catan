78bb9c6883787ee3e631ab3cb6e821949daef90e
Nico <rtrozzo@itba.edu.ar>
2019-02-04 14:44:11 -0300

    M

--- catanFuckYeah/catanFuckYeah/netwBankTradeController.cpp
+++ catanFuckYeah/catanFuckYeah/netwBankTradeController.cpp
@@ -1,9 +1,10 @@
 #include "netwBankTradeController.h"
 #include "playingFSM.h"
 
-netwBankTradeController::netwBankTradeController(catanGameModel*game)
+netwBankTradeController::netwBankTradeController(catanGameModel*game, netwEmisor * emisor_)
 {
 	gameModel = game;
+	emisor = emisor_;
 }
 
 bool netwBankTradeController::parseNetworkingEvent(networkingEv * package)
@@ -15,6 +16,10 @@ bool netwBankTradeController::parseNetworkingEvent(networkingEv * package)
 		if (!(ret = gameModel->bankTrade(bankPckg->getOppResources(), static_cast<resourceType>(bankPckg->getBankResource()))))
 		{
 			controllerEvent = new playingFSMErrorEv("Error en lo que se recibe del trueque del oponente con el banco");
+		}
+		else
+		{
+			emisor->sendPackage(ACK);
 		}
 	}
 	return ret;
--- catanFuckYeah/catanFuckYeah/netwBankTradeController.h
+++ catanFuckYeah/catanFuckYeah/netwBankTradeController.h
@@ -1,13 +1,16 @@
 #pragma once
 #include "EDANetworkingController.h"
 #include "catanGameModel.h"
+#include "netwEmisor.h"
+
 class netwBankTradeController : public EDANetworkingController
 {
 public:
-	netwBankTradeController(catanGameModel * game);
+	netwBankTradeController(catanGameModel * game,netwEmisor * emisor_);
 	bool parseNetworkingEvent(networkingEv * package);
 	netwControllerTypes getType() { return CTRL_BANKTRADE; }
 	~netwBankTradeController();
 private:
 	catanGameModel *gameModel;
+	netwEmisor * emisor;
 };
