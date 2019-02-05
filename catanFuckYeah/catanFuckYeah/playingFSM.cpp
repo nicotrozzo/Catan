df8f0f875fe44e25a11c05c41419654c94785741
Nico <rtrozzo@itba.edu.ar>
2019-02-05 18:25:45 -0300

    d

--- catanFuckYeah/catanFuckYeah/playingFSM.cpp
+++ catanFuckYeah/catanFuckYeah/playingFSM.cpp
@@ -20,7 +20,6 @@ using namespace std;
 playingFSM::playingFSM(bool iStart, catanGameModel * game, std::vector<EDAInputController *> inputControllers, std::vector<EDANetworkingController *> networkingControllers, netwEmisor * em) : genericFSM(&fsmTable[0][0], 8, 5, iStart ? MY_TURN : OPP_TURN), allInputControllers(inputControllers) , allNetworkingControllers(networkingControllers)
 {
 	gameModel = game;
-	robberfsm = nullptr;
 	emisor = em;
 	mapView = new mapViewer(game->getMap());
 	gameModel->attach(mapView);
@@ -36,7 +35,6 @@ playingFSM::playingFSM(bool iStart, catanGameModel * game, std::vector<EDAInputC
 		gameModel->attach(p1view);
 		p2view = new player2Viewer(gameModel->getOtherPlayer());
 		gameModel->attach(p2view);
-		//inputControllerList.push_back(new );
 	}
 	else
 	{
@@ -131,64 +129,6 @@ playingFSM::~playingFSM()
 }
 
 /*ACTION ROUTINES FOR FSM*/
-
-/*
-void playingFSM::sendDices(genericEvent * ev)
-{
-	//mandarle dados al modelo, el modelo debe cambiar los recursos de cada jugador
-	//if (!catanGame->dicesThrown(dices))	//si no gano
-	//{
-		expectedPackages.clear();
-		expectedPackages = { SETTLEMENT, ROAD, CITY, BANK_TRADE, OFFER_TRADE , PASS};
-	//}
-	//else								//si con esos dados gano el otro
-	//{
-	//	fsmEvent = new doneEv; 
-	//	mandar YOU_WON
-	//	expectedeEvents = { PLAY_AGAIN, GAME_OVER };
-	//}
-}
-*/
-
-/*
-void playingFSM::prepareMyRobber(genericEvent * ev)
-{
-	expectedPackages.clear();
-	robberfsm = new myRobberFSM(WAIT_OPPONENT_ANSWER);
-	if(catanGame->otherPlayer->cardsCount() > 7)
-	{
-		expectedPackages = {ROBBER_CARDS, ACK};
-	}
-	else
-	{
-		expectedPackages = {ACK};
-	}
-	
-}
-*/
-
-/*
-void playingFSM::prepareOppRobber(genericEvent * ev)		
-{
-	expectedPackages.clear();
-	if(catanGame->myPlayer->cardsCount() > 7)
-	{
-		robberfsm = new oppRobberFSM(WAIT_MY_USER);
-		expectedPackages = {ROBBER_CARDS, ROBBER_MOVE};
-	}
-	else if(catanGame->otherPlayer->cardsCount() > 7)
-	{
-		robberfsm = new oppRobberFSM(WAIT_OPPONENT);
-		expectedPackages = {ROBBER_CARDS, ROBBER_MOVE};
-	}
-	else
-	{
-		robberfsm = new oppRobberFSM(WAIT_OPPONENT);
-		expectedPackages = {ROBBER_MOVE};
-	}	
-	
-}
-*/
 
 void playingFSM::passControllers(genericEvent * ev)
 {
@@ -290,6 +230,8 @@ void playingFSM::myCardsSent(genericEvent * ev)
 
 void playingFSM::prepareRobbMove(genericEvent * ev)
 {
+	currentInputControllers.clear();
+	currentNetworkingControllers.clear();
 	myRobberMove();
 }
 
@@ -300,11 +242,6 @@ void playingFSM::waitRobbMove(genericEvent * ev)
 
 void playingFSM::error(genericEvent * ev)
 {
-	//ver si sigue igual lo de robber fsm
-	/*if (robberfsm != nullptr)	
-	{
-		delete robberfsm;
-	}*/
 	fsmEvent = new outEv(static_cast<playingFSMEvent *>(ev)->getInfo());
 	//destruir todo lo que tenga que destruir
 }
@@ -515,110 +452,3 @@ void playingFSM::myRobberMove()
 	currentNetworkingControllers.push_back(controllerToAdd);
 	currentInputControllers.push_back(getInputController(CTRL_HEXAGON));
 }
-
-/*void playingFSM::sendToRobberFSM(genericEvent * ev)
-{
-	
-	//robberfsm->cycle(static_cast<stay_ev *>(ev)->additionalEvent)
-	
-}*/
-
-/*void playingFSM::sendTradeOffer(genericEvent * ev)
-{
-	expectedPackages.clear();
-	//catanGame->tradeOffered(tradeOffer)
-	
-}*/
-
-/*void playingFSM::sendAnswer(genericEvent * ev)
-{
-
-}*/
-
-/*void playingFSM::changeTurn(genericEvent * ev)
-{
-	//avisar que cambio el turno
-	//
-}*/
-
-/*void playingFSM::validate(genericEvent * ev)
-{
-
-}*/
-
-
-
-/*oppRobberFSM::oppRobberFSM(stateTypes initState) : genericFSM(&fsmTable[0][0], 3, 2, initState)
-{
-	//expectedPackage = ? ; porque a priori no sabria si tengo que espara input por la seleccion de cartas o solo mandar ack y espera respuesta del otro
-	//deberia arrancar en wait_user y que ahi me fij si tengo que descartar cartas o no
-}
-
-void oppRobberFSM::error(genericEvent * ev)
-{
-	fsmEvent = new unexpectedNetwEvent("Unexpected event during Robber state");
-	//borrar lo que haya que borrar
-}
-
-void oppRobberFSM::savePackage(genericEvent * ev)	//manda robber cards o robber move del oponente al modelo
-{
-	if (ev->getHeader() == ROBBER_CARDS)
-	{
-		catanGameModel->discardOtherPlayer(static_cast<robberCardsPckg *>(ev)->getCards());
-	}
-	else if (ev->getHeader() == ROBBER_MOVE)
-	{
-		catanGame->robberMoved(static_cast<robberMovePckg *>(ev)->getNewRobberPos());
-	}
-}
-
-void oppRobberFSM::sendMyCards(genericEvent * ev)
-{
-	//mandar robber cards propias al oponente
-	new 
-}
-
-
-
-
-void myRobberFSM::savePackage(genericEvent * ev)		//manda al modelo ack o cantidad de cartas seleccionadas
-{
-	if (ev->getHeader() == ACK)
-	{
-		catanGameModel->otherPlayer->discardOtherPlayer("");
-	}
-	else if (ev->getHeader() == ROBBER_CARDS)
-	{
-		catanGameModel->otherPlayer->discardOtherPlayer(static_cast<robberCardsPckg *>(ev)->getCards());
-	}
-}
-
-void myRobberFSM::checkMyMove(genericEvent * ev)
-{
-	//el modelo se va a encargar de habilitar o deshabilitar los controllers para seleccionar las cartas o el robber
-	if (ev->getDescription() == ROBBER_SELECTED)	//esperar eventos de input
-	{
-		catanGame->myPlayer->robberMoved(ev->additionalInfo);
-		packageToSend = ev;
-	}
-	else if (ev->getDescription() == CARD_SELECTED)
-	{
-		catanGame->myPlayer->selectCard(ev->additionalInfo);
-		packageToSend = ev;
-	}
-}
-
-void myRobberFSM::sendMyMove(genericEvent * ev)
-{
-	//podria checkear que el evento sea el tick y no ack....
-	//mandar al empaquetador de paquetes el packageToSend presente y que este como tiene acceso al modelo vea la cantidad de cartas seleccionadas para poder armar el paquete
-	if (packageToSend->getDescription() == ROBBER_MOVE)
-	{
-		//crear evento de "ok" para salir de robberFSM
-		new robberMovePckg(packageToSend->additionalInfo);		//mando el paquete a la otra maquina
-	}
-	else if (packageToSend->getDescription() == ROBBER_CARDS)
-	{
-		new robberCardsPckg(packageToSend->additionalInfo);		//mando el paquete a la otra maquina
-	}
-}*/
--- catanFuckYeah/catanFuckYeah/playingFSM.h
+++ catanFuckYeah/catanFuckYeah/playingFSM.h
@@ -56,39 +56,10 @@ private:
 
 #define TX(x) (static_cast<void (genericFSM::* )(genericEvent *)>(&playingFSM::x)) //casteo a funcion, por visual
 
-/*	const fsmCell fsmTable[8][6] = {	
-	//			OK								CANCEL								ROBBER								BUILD 									TRADE							UNEXPECTED_EVENT					
-	{ { WAITING_DICES,TX(sendPass) },	  { MY_TURN,TX(nada) },				 { MY_ROBBER,TX(prepareMyRobber) },  { BUILDING,TX(mandoconstruccion) },{ PREPARE_TRADE,TX(verdesp) },			{ MY_TURN,TX(error) } },		 //MY_TURN
-	{ { BUILDING,TX(JFSDJFLtHJS) },		  { MY_TURN,TX(cancelOperation) },   { BUILDING,TX(error) },		   { MY_TURN,TX(confirmOperation) },	{ BUILDING,TX(error) },					{ BUILDING,TX(error) } },		 //BUILDING
-	{ { PREPARE_TRADE,TX(OfferOrconfirm)},{ MY_TURN,TX(cancelOpSepararcasos) },{ PREPARE_TRADE,TX(error) },		{ PREPARE_TRADE,TX(error) },		{ MY_TURN,TX(verdesp) },				{ PREPARE_TRADE,TX(error) } },   //PREPARE_TRADE
-	{ { MY_ROBBER,TX(sendToRobber) },	  { MY_TURN,TX(verdesp) },			 { MY_ROBBER,TX(sendToRobber) },   { MY_ROBBER,TX(error) },				{ MY_ROBBER,TX(error) },				{ MY_ROBBER,TX(error) } },		 //MY_ROBBER
-	{ { OPP_TURN,TX(sendDices) },		  { WAITING_DICES,TX(error) },		 { WAITING_DICES,TX(error) },	   { WAITING_DICES,TX(error) },			{ WAITING_DICES,TX(error) },			{ WAITING_DICES,TX(error) } },	 //WAITING_DICES
-	{ { OPP_TURN,TX(moveRobber) },		  { OPP_ROBBER ,TX(verdesp)},		 { OPP_ROBBER,TX(sendToRobberFSM)},{ OPP_ROBBER,TX(error) },			{ OPP_ROBBER,TX(error) },				{ OPP_ROBBER,TX(error) } },		 //OPP_ROBBER
-	{ {	MY_TURN,TX(changeTurn) },		  { OPP_TURN,TX(error) },			 { OPP_ROBBER,TX(prepareOppRobber) }, { OPP_TURN,TX(validate),			{ WAITING_ANSWER,TX(sendTradeOffer) },	{ OPP_TURN,TX(error) } } },		 //OPP_TURN
-	{ { OPP_TURN,TX(sendAnswer) },		  { OPP_TURN,TX(sendAnswer2) },		 { WAITING_ANSWER,TX(error) },	   { WAITING_ANSWER,TX(error) },		{ WAITING_ANSWER,TX(error) },			{ WAITING_ANSWER,TX(error) } }	 //WAITING_ANSWER
-	};
-
-	//The action routines for the FSM
-	void sendDices(genericEvent * ev);
-	void prepareMyRobber(genericEvent * ev);
-	void prepareOppRobber(genericEvent * ev);
-	void error(genericEvent * ev);
-	void sendToRobberFSM(genericEvent * ev);
-	void sendTradeOffer(genericEvent * ev);
-	void sendAnswer(genericEvent * ev);
-	void changeTurn(genericEvent * ev);
-	void validate(genericEvent * ev);
-	void doNothing(genericEvent * ev) {}
-	
-	list<networkingEventTypes> expectedPackages;*/
-	genericFSM * robberfsm;
 	const fsmCell fsmTable[8][5] = {
 	//			CHANGE_STATE							CARDS_EV									TICK_EV							ROBBER_EV							ERROR	
-																					//lo emite el edgeandvertex controller
-	{ { WAITING_DICES,TX(passControllers)},	{PREPARE_TRADE,TX(tradeControllers)},	{BUILDING,TX(buildControllers)},		{MY_ROBBER,TX(doNothing)},	{MY_TURN,TX(error)} },		 //MY_TURN
-																					//lo emite el tick controller																																	 //lo emite el tick controller																																	 //lo emite el tick controller									 	
+	{ { WAITING_DICES,TX(passControllers)},	{PREPARE_TRADE,TX(tradeControllers)},	{BUILDING,TX(buildControllers)},		{MY_ROBBER,TX(doNothing)},	{MY_TURN,TX(error)} },		 //MY_TURN																														 
 	{ { MY_TURN,TX(finishedBuilding) },	{BUILDING,TX(doNothing) },				{BUILDING,TX(ackController)},			{BUILDING,TX(doNothing)},				{BUILDING,TX(error)} },		 //BUILDING
-												//lo emite el tick controller			 //lo emite el tick controller
 	{ { MY_TURN,TX(myTurnControllers) },	{PREPARE_TRADE,TX(ackController)},		{PREPARE_TRADE,TX(netwYNControllers)},	{PREPARE_TRADE,TX(doNothing)},			{PREPARE_TRADE,TX(error)} },	 //PREPARE_TRADE
 	{ { MY_TURN,TX(myTurnControllers) },	{MY_ROBBER,TX(oppCardsReady) },				{MY_ROBBER,TX(robbCardsReady) },	{MY_ROBBER,TX(prepareRobbMove)},	{MY_ROBBER,TX(error)} },		//MY_ROBBER
 	{ { OPP_TURN,TX(oppTurnControllers)},	{WAITING_DICES,TX(doNothing)},			{WAITING_DICES, TX(doNothing)},			{OPP_ROBBER,TX(oppRobberControllers)},	{WAITING_DICES,TX(error)} },	//WAITING_DICES
@@ -144,72 +115,3 @@ public:
 	void sendToNetwControllers(networkingEv *netwPackage);
 	~playingFSM();
 };
-
-/*enum robberStates : stateTypes { WAIT_USER, WAIT_OPPONENT, WAIT_ROBBMOVE };
-
-enum robberFSMEvTypes : eventTypes { INCOMING_ROBBER_CARDS, INCOMING_ROBBER_MOVE, MY_CARDS, MY_CARDS_AND_WAIT };
-
-class robberFSMEv : public genericEvent
-{
-public:
-	robberFSMEv();
-	robberFSMEv(robberFSMEvTypes type_); //if(valid) type(type_) {}
-	robberFSMEvTypes getType() { return type; }
-	bool getError();
-private:
-	//referencia al modelo de juego
-	robberFSMEvTypes type;
-	bool error = false;
-};
-
-class oppRobberFSM : public genericFSM
-{
-private:
-
-#define TX(x) (static_cast<void (genericFSM::* )(genericEvent *)>(&bossFSM::x)) //casteo a funcion, por visual
-
-	const fsmCell fsmTable[2][3] = {
-	//	   PACKAGE							ACCEPT_CLICK						CARD_CLICK									
-	{ { WAIT_USER,TX(error) },		    { WAIT_OPPONENT, TX(sendMyCards) }, { WAIT_MY_USER,TX(takeCard) } },	//WAIT_MY_USER
-	{ { WAIT_OPPONENT,TX(savePackage)}, { WAIT_OPPONENT,TX(error) },		{ WAIT_OPPONENT,TX(error) } },		//WAIT_OPPONENT
-	};
-
-	//The action routines for the FSM
-
-	void error(genericEvent * ev);
-	void sendMyCards(genericEvent * ev);
-	void savePackage(genericEvent * ev);
-	void takeCard(genericEvent * ev);
-
-	//expectedevent
-public:
-	oppRobberFSM(stateTypes initState);	
-};
-
-
-class myRobberFSM : public genericFSM
-{
-private:
-
-#define TX(x) (static_cast<void (genericFSM::* )(genericEvent *)>(&bossFSM::x)) //casteo a funcion, por visual
-
-	const fsmCell fsmTable[2][3] = {
-		//	   PACKAGE							ACCEPT_CLICK							CARD_ROBBER_CLICK									
-		{ { WAIT_MY_USER,TX(savePackage)}, { WAIT_OPPONENT_ANSWER,TX(error) },		{ WAIT_OPPONENT_ANSWER,TX(error) } },	//WAIT_OPPONENT_ANSWER
-		{ { WAIT_MY_USER,TX(error) },	   { WAIT_OPPONENT_ANSWER,TX(sendMyMove) }, { WAIT_MY_ANSWER,TX(checkMyMove) } }	//WAIT_MY_USER
-		};
-
-	//The action routines for the FSM
-
-	void savePackage(genericEvent * ev);
-	void sendMyMove(genericEvent * ev);
-	void checkMyMove(genericEvent * ev);
-	void error(genericEvent * ev);
-	void seveCards(genericEvent * ev);
-	void sendCardsToOpponent(genericEvent * ev);
-	void moveRobber(genericEvent * ev);
-	int oppDiscard;
-	int packageToSend    //paquete a enviar
-public:
-	myRobberFSM(stateTypes initState);
-};*/
